dabac3f7c1d9f7c1557926e42a0946b6467428c5
namitak22 <32341160+namitak22@users.noreply.github.com>
2018-10-16 16:54:56 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 RESOURCE = "https://graph.microsoft.com"  # Add the resource you want the access token for
-TENANT = "Your tenant"  # Enter tenant name, e.g. contoso.onmicrosoft.com
+TENANT = "retail.starbucks.com"  # Enter tenant name, e.g. contoso.onmicrosoft.com
 AUTHORITY_HOST_URL = "https://login.microsoftonline.com"
-CLIENT_ID = "Your client id "  # copy the Application ID of your app from your Azure portal
-CLIENT_SECRET = "Your client secret"  # copy the value of key you generated when setting up the application
+CLIENT_ID = "b015a46c-9686-4fac-95ff-6e88599f15c4"  # copy the Application ID of your app from your Azure portal
+CLIENT_SECRET = "SXMOFHlJeQi6eGRZQqXVxUNe3e/LRlHwQMOl/kxnkpo="  # copy the value of key you generated when setting up the application
 
 # These settings are for the Microsoft Graph API Call
 API_VERSION = 'v1.0'
